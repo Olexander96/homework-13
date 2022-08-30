88b1677f4205f432595b85019822eff78997a85d
Olexander96 <o.sheiko96@gmail.com>
2022-08-30 21:42:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// 1) -  в html
+
+// 2) - створити змінні
+// let name1 = 'Oleksandr';
+// var name2 = 'Olga';
+// const NAME3 = 'Vlad';
+// const NAME4 = 'Julia';
+
+// let 5name = 'Oleksandr17';
+// var -name6 = 'Olga13';
+// const *NAME7 = 'Vladsss';
+// const MY-NAME8 = 'Juliayyy';
+
+// 3) - види коментарів // /**/
+// 4) Наприклад.
+    /*let name;
+    let NAME;
+    let firstItem;
+    let $item;
+    let first_name;*/
+
+// 5) - виведення ім'я
+// let name = prompt('Як тебе звати?');
+// alert('Привіт ' + name + '!');
+
+// 6) - виведення року народження
+// let yearBorn = +prompt('Якого ти року народження?', 0);
+// let yearNow = 2022;
+// let personAge = yearNow - yearBorn;
+// alert('Тобі ' + personAge + ' років!');
+
+// 7) - периметр квадрату
+// let length = +prompt('Яка довжина сторони квадрату, в сантиметрах?', 0);
+// let perimeterSquare = length * 4;
+// alert('Периметр квадрату ' + perimeterSquare + ' сантиметрів!');
+
+// 8) - площа кола
+// let radius = +prompt('Який радіус кола, в сантиметрах?', 0)
+// const PI = 3.14;
+// let area = PI * (radius ** 2);
+// alert(`Площа кола ${area} квадратних сантиметрів!`)
+
+// 9) - визначення швидкості пересування
+// let distance = +prompt('Яка відстань між містами, в кілометрах?', 0);
+// let time = +prompt('За скільки часу Ви хотіли б доїхати, в годинах?');
+// const SPEED = distance / time;
+// alert(`Вам потрібно їхати зі швидкістю ${SPEED} км/год!`)
+
+// 10) - курс валюти
+// let dollar = +prompt('Введіть суму в доларах', 0);
+// const euro = dollar * 0.8;
+// alert(`Ваша сума ${euro} євро`);
+
+
+
+
+
+
+
+
+
+
+
